e84acce60e041ced8a9a5395dbbfe672b0ba0115
Jayce Azua <jayce.azua@gmail.com>
2018-08-18 07:50:49 -0400

    hidding environmental variables

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 const express = require('express');
-
 const http = require('http');
 const https = require('https');
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Global hidden variable API key
-const API_Key = '9229e9f2bf194233a93d5d40a32b50'
+const API_Key = process.env.APIKEY
 
 const yachtAPI = `http://api.boats.com/inventory/search?key=${API_Key}`;
 
